21ce3ba2bd2901bc7610b29f45d92ffa514a0a8e
Spencer Tse <spencertse122@gmail.com>
2022-02-20 11:28:59 -0500

    continue on p.81

--- 004.js
+++ 004.js
@@ -113,3 +113,46 @@ addEntry(["work", "ice cream", "cauliflower", "lasagna", "touched tree", "brushe
 addEntry(["weekend", "cycling", "break", "peanuts", "beer"], true)
 
 console.log(journal2)
+console.log('-'.repeat(10))
+
+// Starting the phi coefficient table
+function phi(table) {
+    return (table[3] * table[0] - table[2] * table[1]) / 
+        Math.sqrt((table[2] + table[3]) *
+                  (table[0] + table[1]) *
+                  (table[1] + table[3]) * 
+                  (table[0] + table[2]))
+}
+
+console.log(phi([76, 9, 4, 1]));
+
+
+function tableFor(event, journal) {
+    let table = [0, 0, 0, 0];
+    for (let i=0; i < journal.length; i++) {
+        let entry = journal[i], index = 0;
+        if (entry.events.includes(event)) index += 1;
+        if (entry.squirrel) index += 2;
+        table[index] += 1;
+    }
+    return table;
+}
+
+console.log(tableFor("pizza", journal))
+
+console.log(journal)
+
+console.log('-'.repeat(10))
+
+// Array Loops
+// regular loop
+for (let i = 0; i < journal.length; i++) {
+    let entry = journal[1];
+    // do something with entry
+}
+// special loop
+for (let entry of journal) {
+    console.log(`${entry.events.length} events.`);
+}
+
+
