ecea86b491f1ba293ae81dfeb93ab1ebf5477b2b
Spencer Tse <spencertse122@gmail.com>
2022-03-07 18:27:50 -0500

    adding empty 007.js for reference

--- 006_borrowingMethod.js
+++ 006_borrowingMethod.js
@@ -20,3 +20,6 @@ let map = {one: true, two: true, hasOwnProperty: true}
 // console.log(map.hasOwnProperty("one"))
 
 console.log(Object.prototype.hasOwnProperty.call(map, "one"))
+console.log(Object.prototype.hasOwnProperty.call(map, "two"))
+console.log(Object.prototype.hasOwnProperty.call(map, "hasOwnProperty"))
+console.log(Object.prototype.hasOwnProperty.call(map, "Wheeee"))
