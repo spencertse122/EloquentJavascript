767e934811d68e0307ab1ca065e7f0cce3a3edab
Spencer Tse <spencertse122@gmail.com>
2022-03-31 23:09:33 -0400

    p.161

---
+++ 009_date.js
@@ -0,0 +1,13 @@
+// The Date Class
+
+console.log(new Date());
+console.log(new Date(2009, 11, 9, 12,59, 59, 999));
+console.log('-'.repeat(10))
+// month number starts at 0!!!!!!
+
+console.log(new Date(2013, 11, 19).getTime())
+// time objects are stored as number of milliseconds 
+// since 1970 in UTC
+
+
+console.log(new Date(1387407600000))
