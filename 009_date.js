e9b8e2f0cd51db3d1abd0508ebf4a2f06e466738
Spencer Tse <spencertse122@gmail.com>
2022-04-02 15:25:22 -0400

    p.169

--- 009_date.js
+++ 009_date.js
@@ -55,4 +55,97 @@ console.log('-'.repeat(10))
 // backtracking
 
 let backtracking = /\b([01]+b|[\da-f]+h|\d+)\b/
-console.log(backtracking.test("1gairoeg"))
+console.log(backtracking.exec("01567926789"))
+
+// the regex will match the first full combo
+// if it fails, it goes back to the starting index 
+
+// The replace method
+console.log('-'.repeat(10))
+console.log(`Replacing 'p' in 'papa': ${"papa".replace("p", "m")}`)
+
+console.log('-'.repeat(10))
+
+console.log(`Replacing 'ou' without g option: ${"Borobudur".replace(/[ou]/, "a")}`)
+console.log(`Replace 'ou' with g option: ${"Borobudur".replace(/[ou]/g, "a")}`)
+
+console.log('-'.repeat(10))
+
+
+// real power of using regular expressions with replace comes from
+// the fact that we can refer to matched groups in the replacement string
+console.log('-'.repeat(10) + "Before Replacement")
+console.log("Liskov, Barbara\nMcCarthy, John\nWadler, Philip")
+
+console.log('-'.repeat(10) + "After Replacement")
+console.log(
+    "Liskov, Barbara\nMcCarthy, John\nWadler, Philip".replace(/(\w+), (\w+)/g, "$2 $1")
+    )
+
+// $1 means replacing the first () group, $2 means the second
+console.log('-'.repeat(10))
+console.log("replace cia and fbi to uppercase with regex")
+let s = "the cia and fbi";
+console.log(s.replace(/\b(fbi|cia)\b/g, str => str.toUpperCase()))
+
+
+// another example
+console.log('-'.repeat(10))
+
+// this is the string we want to play with
+let stock = "1 lemon, 2 cabbages, and 101 eggs";
+
+// this is the function we want to use to modify the string
+function minusOne(match, amount, unit) {
+    // first modify the number amount by -1
+    // because that's what we do 
+    amount = Number(amount) - 1;
+    // if the amount is one, it's singular
+    // we will take away the 's'
+    if (amount == 1) { //only one left ,remove the 's'
+        // slice the string by excluding the last digit
+        unit = unit.slice(0, unit.length - 1);
+    // if the amount is zero, change the amount string to ZERO!!!
+    } else if (amount == 0) {
+        amount = "no";
+    }
+    return amount + " " + unit;
+}
+
+// the regex below finds a digit and a word separated with a space " "
+// and then the replacement takes in a function
+// return a new string with a new number and the same unit
+console.log(stock.replace(/(\d+) (\w+)/g, minusOne));
+
+
+console.log('-'.repeat(10))
+
+// GREED
+
+// this should be stripping out all the comment strings 
+function stripComments(code) {
+    // "\/\/.*|\/\*[^]*\*\/" <------ this is the actual one we want to look at 
+    //    //.*    | /*  [anything]wildcard*  */
+    return code.replace(/\/\/.*|\/\*[^]*\*\//g, "")
+}
+
+console.log(stripComments("1 + /* 2 */3")) // the /* 2 */ is a comment
+// so this should return a 1 + 3
+console.log(stripComments("x = 10;// ten!"))
+console.log(stripComments("1 /* a */+/* b */ 1")) // /* a */ and /* b */ are comments
+
+
+// NON GREEDY!!!!!
+
+function stripComments(code) {
+    return code.replace(/\/\/.*|\/\*[^]*?\*\//g, "");
+}
+console.log(stripComments("1 /* a */+/* b */ 1"))
+
+
+// dynamically creating regexp objects
+let name = "harry";
+let text = "Harry is a suspicious character."
+let regexp = new RegExp("\\b(" + name + ")\\b", "gi");
+console.log(text.replace(regexp, "_$1_"))
+console.log(regexp)
