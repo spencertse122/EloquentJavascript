0308efe38a0bfe8e051ffe6ec0bd07def3d10a14
Spencer Tse <spencertse122@gmail.com>
2022-04-05 12:54:19 -0400

    make up work

--- 010.js
+++ 010.js
@@ -55,3 +55,19 @@ exports.formatDate = function(date, format) {
         if (tag == "dddd") return days[date.getDay()]
     })
 }
+
+// another usage format
+
+require.cache = Object.create(null);
+
+function require(name) {
+    if (!(name in require.cache)) {
+        let code = readFile(name);
+        let module = {exports: {}};
+        require.cache[name] = module;
+        let wrapper = Function("require, exports, module", code) 
+        wrapper(require, module.exports, module);
+    }
+    return require.cache[name].exports;
+}
+
