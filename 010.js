5a1ef2d9ba021a9d715c9b9445e365b9337aafa8
Spencer Tse <spencertse122@gmail.com>
2022-04-09 23:17:47 -0400

    module design p.188

--- 010.js
+++ 010.js
@@ -85,3 +85,9 @@ import {days, months} from "date-names";
 export function formatDate(date, format) { /* ... */ }
 
 export default ["Winter", "Spring", "Summer", "Autumn"]
+
+
+import {days as dayNames} from "date-names";
+
+console.log(dayNames.length);
+
