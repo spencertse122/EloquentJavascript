fd8f33a1452411cfbf9c76405f7d06865e99f2f8
Spencer Tse <spencertse122@gmail.com>
2022-04-12 23:42:41 -0400

    p.192

--- 010_exercise_modularRobot.js
+++ 010_exercise_modularRobot.js
@@ -15,4 +15,37 @@
 // you create? Which module would depend on which other module, and what
 // would their interfaces look like?
 // Which pieces are likely to be available prewritten on NPM? Would you prefer
-// to use an NPM package or write them yourself?
+// to useRadiology Partners
+
+const roads = [
+    "Alice's House-Bob's House",
+    "Alice's House-Cabin",
+    "Alice's House-Post Office",
+    "Bob's House-Town Hall",
+    "Daria's House-Ernie's House",
+    "Daria's House-Town Hall",
+    "Ernie's House-Grete's House",
+    "Grete's House-Farm",
+    "Grete's House-Shop",
+    'Marketplace-Farm',
+    'Marketplace-Post Office',
+    'Marketplace-Shop',
+    'Marketplace-Town Hall',
+    'Shop-Town Hall'
+  ];
+
+const mailRoute = [
+    "Alice's House",
+    'Cabin',
+    "Alice's House",
+    "Bob's House",
+    'Town Hall',
+    "Daria's House",
+    "Ernie's House",
+    "Grete's House",
+    'Shop',
+    "Grete's House",
+    'Farm',
+    'Marketplace',
+    'Post Office'
+];
